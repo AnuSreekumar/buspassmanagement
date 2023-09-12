927785736d9bd496cd6917c96b76f26cc78bc559
Anu Sreekumar <118008148+AnuSreekumar@users.noreply.github.com>
2023-09-12 21:27:49 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+
+	<div class="copy-right"> 
+		<div class="container">
+			<center><span style="color: white">&#169; <?php echo date("Y"); ?> All rights reserved. Made with &#x1F49C; by <b><a href="https://www.youtube.com/channel/UC4_6-VSWBw_QHMyjrDDEvVQ"> GPW</a></b> Team.</span></center>
+		</div> 
+	</div> 
+	<!-- //footer -->   
+	
